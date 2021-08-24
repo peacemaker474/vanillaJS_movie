e4c2c127cefe4b10c8e74cf99077492c28fdc3aa
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-08-24 19:53:45 +0900

    2021.08.24. API Request

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "a9dc1bd31af50bb46c99983404356308";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: API_KEY,
+        language: "ko-KR"
+    }
+});
+
+const getAPI = {
+    upcoming: () => api.get("movie/upcoming"),
+}
+
+getAPI.upcoming();
