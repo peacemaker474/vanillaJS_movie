640620dc23c87f7cd8bc1979a8785bd2a6ee1815
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-08-31 01:04:54 +0900

    2021.08.30. webpack dev server build

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
         filename: "[name].js",
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        port: 9000,
+    },
     module: {
         rules: [
             {
